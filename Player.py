5b4b13a875bf932849e3e3df45ff6638ef94cd8f
Wendy Onwuagana <uchewendy10@gmail.com>
2024-09-28 11:26:56 -0500

    created player, decision and narrative classes

---
+++ Player.py
@@ -0,0 +1,20 @@
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.decisions = []  
+        self.decisionPoints = 0  
+
+    def addDecision(self, decision: 'Decision'):
+        from Decision import Decision
+        self.decisions.append(decision)
+
+    def getDecisions(self):
+        return self.decisions
+
+    def awardDecisionPoints(self):
+        self.decisionPoints += 1
+
+    def getDecisionPoints(self):
+        return self.decisionPoints
+
