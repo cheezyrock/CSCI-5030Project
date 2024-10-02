de3df725671bb32117dd0fa3c32b2e0b2fcbc102
chris-b4 <chris.brault@slu.edu>
2024-10-02 00:49:15 -0500

    Added Audio.py file (hopefully I did it right...)

---
+++ Audio.py
@@ -0,0 +1,7 @@
+# Create audio_sounds class
+    # Create method for background music
+    # Create methods for each sound effect
+        # If SFX is already playing, don't play another
+# Create AudioControls class
+    # Create master_volume method
+    # Create sound_effects_volume method
