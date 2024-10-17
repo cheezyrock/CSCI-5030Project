4c95dfa8c838f806e0462fbeecab4156a764dc58
Cheezyrock <cheezyrock@gmail.com>
2024-10-17 12:44:09 -0500

    Code Cleanup

--- image_handler.py
+++ image_handler.py
@@ -1,9 +1,14 @@
 from os import path
+from pickle import NONE
 
 from PIL import Image, ImageTk
 class ImageHandler:
     @staticmethod
     def load_image(image_path, size=(300,300)):
-        img = Image.open(image_path)
-        img = img.resize(size, Image.Resampling.LANCZOS)
-        return ImageTk.PhotoImage(img)
+        try:
+            img = Image.open(image_path)
+            img = img.resize(size, Image.Resampling.LANCZOS)
+            return ImageTk.PhotoImage(img)
+        except:
+            #Ignore the error for now.  This needs better error handling.
+            return NONE
